e9f7b4c1b93eaeb3428ce913bceb0a4eb0f376c4
Sean Connolly <connolly.st@gmail.com>
2014-05-30 22:39:11 +0200

    Cleaned up code formatting

--- double_opponent/+utils/distance_xop.m
+++ double_opponent/+utils/distance_xop.m
@@ -1,13 +1,10 @@
-function [d]=distance_xop(xx,yy,type)
-
-
-switch type
-	case ('eucl')
-		d=sqrt(xx.*xx+yy.*yy);
-	case ('manh')
-		d=abs(xx)+abs(yy);
-	otherwise
-		disp ('ERRORRRR!!!! La distancia no esta ben especificada!!!!!');
-end
-
+function d = distance_xop(xx, yy, type)
+    switch type
+        case ('eucl')
+            d = sqrt(xx.*xx + yy.*yy);
+        case ('manh')
+            d = abs(xx) + abs(yy);
+        otherwise
+            error('Unsupported distance calculation type: %s', type);
+    end
 end
