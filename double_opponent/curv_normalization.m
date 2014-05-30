102c0635ac69a826ee408cbde697da975e50e7a4
Sean Connolly <connolly.st@gmail.com>
2014-05-30 22:57:06 +0200

    translated to english

--- double_opponent/curv_normalization.m
+++ double_opponent/curv_normalization.m
@@ -1,4 +1,4 @@
-function [dades,normal_max,normal_min] = curv_normalization(dades, struct)
+function [data,normal_max,normal_min] = curv_normalization(data, struct)
 
 factor_normal=struct.zli.normal_input;
 type=struct.zli.normal_type;
@@ -6,8 +6,8 @@ type=struct.zli.normal_type;
 	% normalitzacio
 	shift=struct.zli.shift;
 
-	ncells=size(dades,1);
-	n_scales=size(dades{1},3);
+	ncells=size(data,1);
+	n_scales=size(data{1},3);
 	
 switch type
 	case ('all')
@@ -16,19 +16,19 @@ switch type
 		normal_min_v=zeros(ncells,1);
 		
 		for i=1:ncells
-			normal_max_v(i)=max(dades{i}(:),[],1);
-			normal_min_v(i)=min(dades{i}(:),[],1);
+			normal_max_v(i)=max(data{i}(:),[],1);
+			normal_min_v(i)=min(data{i}(:),[],1);
 		end
 		
 		normal_max=max(normal_max_v(:),[],1);
 		normal_min=min(normal_min_v(:),[],1);
 		
 		if normal_max==normal_min
-			dades{i}=1;
+			data{i}=1;
 		else
 			
 			for i=1:ncells
-				dades{i}=((dades{i}-normal_min)/(normal_max-normal_min))*(factor_normal-shift)+shift;
+				data{i}=((data{i}-normal_min)/(normal_max-normal_min))*(factor_normal-shift)+shift;
 			end
 		end
 			% -------------------------------
@@ -38,7 +38,7 @@ switch type
 		normal_min_v=zeros(n_scales,ncells);
 		for s=1:n_scales
 			for i=1:ncells
-				kk=dades{i}(:,:,s,:);
+				kk=data{i}(:,:,s,:);
 				normal_max_v(s,i)=max(kk(:),[],1);
 				normal_min_v(s,i)=min(kk(:),[],1);
 			end
@@ -49,11 +49,11 @@ switch type
 		for s=1:n_scales
 			if normal_max(s)==normal_min(s)
 				for i=1:ncells
-					dades{i}(:,:,s,:)=1.02; % El minim segons Li1998
+					data{i}(:,:,s,:)=1.02; % El minim segons Li1998
 				end
 			else
 				for i=1:ncells
-					dades{i}(:,:,s,:)=((dades{i}(:,:,s,:)-normal_min(s))/(normal_max(s)-normal_min(s)))*(factor_normal-shift)+shift;
+					data{i}(:,:,s,:)=((data{i}(:,:,s,:)-normal_min(s))/(normal_max(s)-normal_min(s)))*(factor_normal-shift)+shift;
 				end
 			end
 		end
@@ -63,14 +63,14 @@ switch type
 		normal_min=struct.zli.normal_min_absolute; 
 		normal_max=struct.zli.normal_max_absolute; 
 			for i=1:ncells
-				dades{i}=((dades{i}-normal_min)/(normal_max-normal_min))*(factor_normal-shift)+shift;
+				data{i}=((data{i}-normal_min)/(normal_max-normal_min))*(factor_normal-shift)+shift;
 			end
 
 end
 
 
 for i=1:ncells
-	dades{i}(dades{i}==shift)=0; % Per posar a zero el que era zero inicialment (Li1998)
+	data{i}(data{i}==shift)=0; % Per posar a zero el que era zero inicialment (Li1998)
 end
 	
 end
