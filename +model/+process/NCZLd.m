87896b60b698ad7ef20df3b016bfd5ba112e0124
Sean Connolly <connolly.st@gmail.com>
2014-05-31 21:04:05 +0200

    General refactoring

--- +model/+process/NCZLd.m
+++ +model/+process/NCZLd.m
@@ -1,35 +1,26 @@
-function [img_out] = NCZLd(img, struct)
+function [I_out] = NCZLd(I, config)
+    I = double(I);
+    
     % Track processing time
-    t_ini = tic;
+    start_time = tic;
     
     % Display input image dimensions
-    img_width  = size(img, 1);
-    img_height = size(img, 2);
-    fprintf('Image size: %ix%i\n', img_width, img_height);
+    I_width      = size(I, 1);
+    I_height     = size(I, 2);
+    I_dimensions = size(I, 3);
+    fprintf('Image size: %ix%ix%i\n', I_width, I_height, I_dimensions);
     
     %-------------------------------------------------------
-    % get the structure/parameters
-    zli      = struct.zli;
-    compute  = struct.compute;
-    image    = struct.image;
-    n_membr  = zli.n_membr;
-    n_scales = struct.wave.n_scales;
-    mida_min = struct.wave.mida_min;
-    dynamic  = compute.dynamic;
+    zli     = config.zli;
+    n_membr = zli.n_membr;
+    dynamic = config.compute.dynamic;
     %-------------------------------------------------------
     
-    % calculate number of scales (n_scales) automatically
-    if n_scales == 0
-        if zli.fin_scale_offset == 0
-            % parameter to adjust the correct number of the last wavelet plane (obsolete)
-            extra = 2;
-        else
-            extra = 3;
-        end
-        % TODO scales should be calculated using all dimensions
-        n_scales = floor(log(max(size(img(:,:,1))-1)/mida_min)/log(2)) + extra;
+    % Calculate number of scales automatically
+    if config.wave.n_scales == 0
+        config.wave.n_scales = calculate_scales(I, config);    
     end
-    struct.wave.n_scales = n_scales;
+    fprintf('Processing at %i scales\n', config.wave.n_scales);
     
     %-------------------------------------------------------
 
@@ -37,23 +28,30 @@ function [img_out] = NCZLd(img, struct)
     %%%%% NCZLd for every channel %%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    img_out_tmp = model.process.NCZLd_channel_v1_0(double(img), struct);
-
-    % do/don't store img and img_out (warning: img_out is 4D in the dynamical case!)
-    if struct.display_plot.store_img_img_out==1
-        save([image.name, '_img.mat'],'img')
-        save([image.name, '_img_out.mat'],'img_out')
-     end
-
-    % static case
+    I_out = model.process.NCZLd_channel_v1_0(I, config);
+    
+    % Static case
     if dynamic == 0
-        % take the mean (see Li, 1999)
-        n_frames_promig = struct.image.n_frames_promig;
+        % We take the mean as the output, as in Li, 1999
+        n_frames_promig = config.image.n_frames_promig;
         ff_ini          = n_membr-n_frames_promig+1;
         ff_fin          = n_membr;
-        img_out         = mean(img_out_tmp(:,:,ff_ini:ff_fin), 3);
+        I_out           = mean(I_out(:,:,ff_ini:ff_fin), 3);
     end
 
-    % time
-    toc(t_ini)
+    % Print processing time
+    toc(start_time)
+end
+
+function n_scales = calculate_scales(I, config)
+    if config.zli.fin_scale_offset == 0
+        % parameter to adjust the correct number of the last wavelet plane (obsolete)
+        % TODO if this is obsolete, can it be removed?
+        extra = 2;
+    else
+        extra = 3;
+    end
+    mida_min = config.wave.mida_min;
+    % TODO scales should be calculated using all dimensions
+    n_scales = floor(log(max(size(I(:,:,1))-1)/mida_min)/log(2)) + extra;
 end
