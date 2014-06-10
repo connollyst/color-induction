e4e9de8df5c243ab829b255b465f2d6e53679368
Sean Connolly <connolly.st@gmail.com>
2014-06-10 15:58:10 +0200

    More readable variable names

--- +model/+process/Rmodelinductiond_v0_3_2.m
+++ +model/+process/Rmodelinductiond_v0_3_2.m
@@ -10,34 +10,40 @@ function [gx_final] = Rmodelinductiond_v0_3_2(Iitheta, config)
 %
 %   gx_final:   the excitation membrane potentials
 
-    %% get the structure and the parameters
-    wave      = config.wave;
-    zli       = config.zli;
-    n_scales  = wave.n_scales;
-    n_membr   = zli.n_membr;
-    n_iter    = zli.n_iter;
-    Delta     = zli.Delta * utils.scale2size(1:n_scales, zli.scale2size_type, zli.scale2size_epsilon);
+    %% Get the configuration parameters
+    image      = config.image;
+    wave       = config.wave;
+    zli        = config.zli;
+    n_cols     = image.width;
+    n_rows     = image.height;
+    n_channels = image.n_channels;
+    n_scales   = wave.n_scales;
+    n_orients  = wave.n_orients;
+    n_membr    = zli.n_membr;
+    n_iter     = zli.n_iter;
+    Delta      = zli.Delta * utils.scale2size(1:n_scales, zli.scale2size_type, zli.scale2size_epsilon);
 
-    %% Initialize parameters
-    [M, N, K]            = MNK(Iitheta);
-    % output membrane potentials
-    [gx_final, gy_final] = initialize_output(M, N, K, n_membr, n_scales);
-
-    %% Normalize input data
-    Iitheta = model.normalize_input(Iitheta, config);
+    if n_cols <= 10 || n_rows <= 10
+       error('Bad stimulus dimensions: the toroidal boundary conditions are ill-defined.');
+    end
     
-    %% Prepare normalization mask
-    normalization_masks = model.terms.get_normalization_masks(M, N, config);
+    %% Initialize output membrane potentials
+    [gx_final, gy_final] = initialize_output(n_membr, n_scales, n_orients, n_cols, n_rows, n_channels);
 
-    %% Prepare orientation/scale interactions for x_ei
+    %% Normalization
+    Iitheta = model.normalize_input(Iitheta, config);
+    normalization_masks = model.terms.get_normalization_masks(n_cols, n_rows, config);
+
+    %% Prepare orientation/scale/color interactions for x_ei
     interactions = model.terms.get_interactions(Delta, config);
     
     %% Prepare J & W: the excitatory and inhibitory masks
-    JW = model.terms.get_JW(M, N, K, Delta, interactions.radius_sc, config);
+    % TODO perhaps J & W don't need the interactions.radius_sc?
+    JW = model.terms.get_JW(n_cols, n_rows, n_orients, Delta, interactions.radius_sc, config);
 
     %% Preallocate x & y: the excitation and inhibition activity
-    x = Iitheta{1};                 % initialized as the visual stimulus (p.192)
-    y = zeros(M, N, n_scales, K);   % initialized with zero activity
+    x = Iitheta{1, 1, 1};                           % initialized as the visual stimulus (p.192)
+    y = zeros(n_cols, n_rows, n_scales, n_orients); % initialized with zero activity
     
     %% Run recurrent network: the loop over time
     for t_membr=1:n_membr  % membrane time
@@ -46,7 +52,8 @@ function [gx_final] = Rmodelinductiond_v0_3_2(Iitheta, config)
         for t_iter=1:n_iter  % from the differential equation (Euler!)
             fprintf('Membrane interation: %i/%i\n', t_iter, n_iter);
             [x, y] = model.process.UpdateXY(...
-                        t_membr, Iitheta, x, y, M, N, K, Delta, JW,...
+                        t_membr, Iitheta, x, y, n_cols, n_rows, n_orients,...
+                        Delta, JW,...
                         normalization_masks, interactions, config...
                      );
         end
@@ -55,45 +62,37 @@ function [gx_final] = Rmodelinductiond_v0_3_2(Iitheta, config)
         gy_final{t_membr} = model.terms.newgy(y);
     end
 
-    for i=1:n_membr
-        gx_final_2=gx_final{i}(:,:,:,2);
-        gx_final{i}(:,:,:,2)=gx_final{i}(:,:,:,3);
-        gx_final{i}(:,:,:,3)=gx_final_2;
+    % TODO some kind of normalization specific to 3 orientations, refactor
+    for t=1:n_membr
+        for s=1:n_scales
+            gx_final_2 = gx_final{t,s,2}(:,:,:);
+            gx_final{t,s,2}(:,:,:) = gx_final{t,s,3}(:,:,:);
+            gx_final{t,2,3}(:,:,:) = gx_final_2;
+        end
     end
 
 end
 
-function [M, N, K] = MNK(Iitheta)
-%MNK Extract M, N, & K properties of the input data
-%   Iitheta:    the input data
-%   
-%   M:          the input data width (cols)
-%   N:          the input data height (rows)
-%   K:          the number of neuron pairs in each hypercolumn
-%                   (i.e. the number of preferred orientations)
-    M = size(Iitheta{1}, 1);
-    N = size(Iitheta{1}, 2);
-    K = size(Iitheta{1}, 4);
-    if M <= 10 || N <= 10
-       disp('Bad stimulus dimensions! The toroidal boundary conditions are ill-defined.')
-    end
-end
-
-function [gx_final, gy_final] = initialize_output(M, N, K, n_membr, n_scales)
+function [gx_final, gy_final] = initialize_output(n_membr, n_scales, n_orients, n_cols, n_rows, n_channels)
 %INITIALIZE_OUTPUT Initialize the output data structures
-%   M:          the input data width
-%   N:          the input data height
-%   K:          the number of preferred orientations
 %   n_membr:    the number of membrane time steps being processed
 %   n_scales:   the number of scale sizes being processed
+%   n_orients:  the number of preferred orientations
+%   n_cols:     the number of cols in the input images (width)
+%   n_rows:     the number of rows in the input images (height)
+%   n_channels: the number of color channels in the input images
 %
 %   gx_final:   the excitation membrane potentials
 %   gy_final:   the inhibition membrane potentials
 
-    gx_final = cell(n_membr, 1);
-    gy_final = cell(n_membr, 1);
-    for i=1:n_membr
-        gx_final{i} = zeros(M, N, n_scales, K); 
-        gy_final{i} = zeros(M, N, n_scales, K);
+    gx_final = cell(n_membr, n_scales, n_orients);
+    gy_final = cell(n_membr, n_scales, n_orients);
+    for t=1:n_membr
+        for s=1:n_scales
+            for o=1:n_orients
+                gx_final{t,s,o} = zeros(n_cols, n_rows, n_channels); 
+                gy_final{t,s,o} = zeros(n_cols, n_rows, n_channels);
+            end
+        end
     end
 end
