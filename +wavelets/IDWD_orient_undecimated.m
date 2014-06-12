2eed33fc8ef6b5dc6f841fa2f4229b6886b66770
Sean Connolly <connolly.st@gmail.com>
2014-06-13 00:51:36 +0200

    Upgraded inverse DWD wavelet to handle n channels

--- +wavelets/IDWD_orient_undecimated.m
+++ +wavelets/IDWD_orient_undecimated.m
@@ -1,7 +1,7 @@
 function rec = IDWD_orient_undecimated(w, c)
-    wlev = length(c);
-    rec  = c{wlev,1};
-    for s = wlev:-1:1
-        rec = rec + w{s,1}(:,:,1) + w{s,1}(:,:,2) + w{s,1}(:,:,3);
+    n_scales = length(c);
+    rec      = c{1,n_scales};
+    for s = n_scales:-1:1
+        rec = rec + w{1,s} + w{2,s} + w{3,s};
     end
 end
