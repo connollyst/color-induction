092a3726790002b4eb0498ea62e66a7965bcf884
Sean Connolly <connolly.st@gmail.com>
2014-06-14 20:42:28 +0200

    Corrected curv_final calculation
    Also:
    Removed unused functions
    Removed unused return variables

--- +model/+process/NCZLd_channel_ON_OFF_v1_1.m
+++ +model/+process/NCZLd_channel_ON_OFF_v1_1.m
@@ -1,24 +1,10 @@
-function [curv_final_out, curv_ON_final, curv_OFF_final, iFactor_ON, iFactor_OFF] = ...
-                                                NCZLd_channel_ON_OFF_v1_1(curv_in, config)
+function curv_final_out = NCZLd_channel_ON_OFF_v1_1(curv, config)
 %NCZLd_CHANNEL_ON_OFF_V1_1 Separate ON and OFF channels and start
 %   recovering thye response at the level of the wavelet/Gabor responses.
 % 
 
-    % preallocate
-    % TODO I don't like this, we should preallocate with zeros
-    curv_final_out = curv_in;
-
-    %-------------------------------------------------------
-    display_plot  = config.display_plot;
-    n_membr       = config.zli.n_membr;
     ON_OFF        = config.zli.ON_OFF;
     fin_scale     = config.wave.fin_scale;
-    n_orients      = config.wave.n_orients;
-    plot_wavelet_planes = display_plot.plot_wavelet_planes;
-    %-------------------------------------------------------
-
-    % TODO is this necessary? Is MATLAB pass by reference or value?
-    curv = curv_in;
     
     % Number of scales
     % TODO this is super sloppy..
@@ -27,44 +13,20 @@ function [curv_final_out, curv_ON_final, curv_OFF_final, iFactor_ON, iFactor_OFF
     % choose the algorithm (separated, abs, quadratic) 
     switch(ON_OFF)
         case 0 % Separated
-            [curv_final, curv_ON_final, curv_OFF_final, iFactor_ON, iFactor_OFF] = ...
-                process_ON_OFF_separately(curv, config);
+            curv_final = process_ON_OFF_separately(curv, config);
         case 1 % ABS
-            [curv_final] = process_ON_OFF_abs(curv, config);
+            curv_final = process_ON_OFF_abs(curv, config);
         case 2 % Square (quadratic)
-            [curv_final] = process_ON_OFF_square(curv, config);
+            curv_final = process_ON_OFF_square(curv, config);
     end
 
-    for t=1:n_membr
-        for s=1:fin_scale
-            for o=1:n_orients
-                % TODO can we just assign, without the loops?
-                curv_final_out{o,s,t} = curv_final{o,s,t}(:,:,:);
-            end
-        end
-    end
-
-    % save raw values for all
-    if config.display_plot.store == 1
-        write_data(curv, curv_final, ...
-            curv_ON, curv_OFF, curv_ON_final, curv_OFF_final, ...
-            iFactor_ON, iFactor_OFF, iFactor, ...
-            config ...
-        );
-    end
-
-    % display for debuging
-    if plot_wavelet_planes == 1
-        figure;
-        subplot(1,3,1), imagesc(curv{orient, scale, n_iter});       colormap('gray');
-        subplot(1,3,2), imagesc(iFactor(:,:,n_iter), [0 1]);        colormap('gray');
-        subplot(1,3,3), imagesc(curv_final{orient, scale, n_iter}); colormap('gray');
-    end
+    curv_final_out = cell(size(curv));
+    curv_final_out(:,fin_scale+1,:) = curv(:,fin_scale+1,:);
+    curv_final_out(:,1:fin_scale,:) = curv_final(:,1:fin_scale,:);
 
 end
 
-
-function [curv_final, curv_ON_final, curv_OFF_final, iFactor_ON, iFactor_OFF] = process_ON_OFF_separately(curv, config)
+function curv_final = process_ON_OFF_separately(curv, config)
 %PROCESS_ON_OFF_SEPARATELY Process the ON and OFF channels independently.
 
     n_membr        = config.zli.n_membr;
@@ -72,25 +34,22 @@ function [curv_final, curv_ON_final, curv_OFF_final, iFactor_ON, iFactor_OFF] = 
     n_orients      = config.wave.n_orients;
     
     %% Initialize data structures
-    index_ON       = cell(n_orients, fin_scale, n_membr);
-    index_OFF      = cell(n_orients, fin_scale, n_membr);
     curv_ON        = curv;
     curv_OFF       = curv;
     curv_ON_final  = curv;
     curv_OFF_final = curv;
-    curv_final     = curv;	 
+    curv_final     = cell(size(curv));
 
     %% Calculate the ON/OFF signals
     for t=1:n_membr
         for s=1:fin_scale
             for o=1:n_orients
-                % TODO we don't need to store the indices outside this loop
-                index_OFF{o,s,t} = find(curv{o,s,t} <= 0);
-                index_ON{o,s,t}  = find(curv{o,s,t} >= 0);
+                index_OFF        = find(curv{o,s,t} <= 0);
+                index_ON         = find(curv{o,s,t} >= 0);
                 curv_ON{o,s,t}   =  curv{o,s,t};
                 curv_OFF{o,s,t}  = -curv{o,s,t};
-                curv_OFF{o,s,t}(index_ON{o,s,t}) = 0;
-                curv_ON{o,s,t}(index_OFF{o,s,t}) = 0;
+                curv_ON{o,s,t}(index_OFF) = 0;
+                curv_OFF{o,s,t}(index_ON) = 0;
             end
         end
     end
@@ -106,10 +65,14 @@ function [curv_final, curv_ON_final, curv_OFF_final, iFactor_ON, iFactor_OFF] = 
     %% Prepare output
     iFactor = iFactor_ON;
     for t=1:n_membr
-        curv_ON_final{t}  =  curv_ON{t}     .* iFactor_ON{t}  * config.zli.normal_output;
-        curv_OFF_final{t} = -curv_OFF{t}    .* iFactor_OFF{t} * config.zli.normal_output;
-        iFactor{t}        = iFactor_ON{t}    + iFactor_OFF{t};
-        curv_final{t}     = curv_ON_final{t} + curv_OFF_final{t};
+        for s=1:fin_scale
+            for o=1:n_orients
+                curv_ON_final{o,s,t}  =  curv_ON{o,s,t}     .* iFactor_ON{o,s,t}  * config.zli.normal_output;
+                curv_OFF_final{o,s,t} = -curv_OFF{o,s,t}    .* iFactor_OFF{o,s,t} * config.zli.normal_output;
+                iFactor{o,s,t}        = iFactor_ON{o,s,t}    + iFactor_OFF{o,s,t};
+                curv_final{o,s,t}     = curv_ON_final{o,s,t} + curv_OFF_final{o,s,t};
+            end
+        end
     end
 end
 
@@ -142,25 +105,5 @@ function [curv_final] = process_ON_OFF_square(curv, config)
     iFactor = Rmodelinductiond_v0_3_2(dades, config);
     for t=1:n_membr
         curv_final{t} = curv{t} .* iFactor{t} * zli.normal_output;
-    end
-end
-
-function write_data(curv, curv_final, curv_ON, curv_OFF, curv_ON_final, curv_OFF_final, iFactor_ON, iFactor_OFF, iFactor, config)
-    image = config.image;
-    save([image.name '_curv'],'curv');
-    save([image.name '_curv_final'],'curv_final');
-    switch(ON_OFF)
-        case 0
-            save([image.name '_curv_ON'],'curv_ON');
-            save([image.name '_curv_OFF'],'curv_OFF');
-            save([image.name '_curv_ON_final'],'curv_ON_final');
-            save([image.name '_curv_OFF_final'],'curv_OFF_final');
-            save([image.name '_iFactor_ON'],'iFactor_ON');
-            save([image.name '_iFactor_OFF'],'iFactor_OFF');
-            save([image.name '_iFactor'],'iFactor');
-        case 1
-            save([image.name '_iFactor'],'iFactor');
-        case 2
-            save([image.name '_iFactor'],'iFactor');
     end
 end
