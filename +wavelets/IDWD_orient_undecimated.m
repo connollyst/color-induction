73966eae493f07f93e6bb3e0415aaca5224fc859
Sean Connolly <connolly.st@gmail.com>
2014-06-14 22:03:25 +0200

    Corrected DWD inverse

--- +wavelets/IDWD_orient_undecimated.m
+++ +wavelets/IDWD_orient_undecimated.m
@@ -1,6 +1,6 @@
 function rec = IDWD_orient_undecimated(w, c)
     n_scales = length(c);
-    rec      = c{1,n_scales};
+    rec      = c{n_scales,1};
     for s = n_scales:-1:1
         rec = rec + w{1,s} + w{2,s} + w{3,s};
     end
