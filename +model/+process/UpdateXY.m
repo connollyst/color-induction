aa95b558ea28b40d1c29ec3d86dfb251972ca095
Sean Connolly <connolly.st@gmail.com>
2014-06-16 13:11:59 +0200

    Noise is generated for each channel

--- +model/+process/UpdateXY.m
+++ +model/+process/UpdateXY.m
@@ -191,8 +191,9 @@ function noise = generate_noise(config)
 %GENERATE_NOISE Generate neural noise.
     n_cols     = config.image.width;
     n_rows     = config.image.height;
+    n_channels = config.image.n_channels;
     n_scales   = config.wave.n_scales;
     n_orients  = config.wave.n_orients;
     var_noise  = 0.1 * 2;
-    noise      = var_noise * (rand(n_cols, n_rows, n_scales, n_orients)) - 0.5;
+    noise      = var_noise * (rand(n_cols, n_rows, n_channels, n_scales, n_orients)) - 0.5;
 end
