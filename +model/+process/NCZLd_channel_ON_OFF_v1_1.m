3efe4d57faedf78baabef8eaa2b7d3a0b360a6ba
Sean Connolly <connolly.st@gmail.com>
2014-06-16 14:19:44 +0200

    Renamed "curv" to "Iitheta"

--- +model/+process/NCZLd_channel_ON_OFF_v1_1.m
+++ +model/+process/NCZLd_channel_ON_OFF_v1_1.m
@@ -1,7 +1,6 @@
-function curv_final = NCZLd_channel_ON_OFF_v1_1(curv, config)
+function Iitheta_final = NCZLd_channel_ON_OFF_v1_1(Iitheta, config)
 %NCZLd_CHANNEL_ON_OFF_V1_1 Separate ON and OFF channels and start
-%   recovering thye response at the level of the wavelet/Gabor responses.
-% 
+%   recovering the response at the level of the wavelet/Gabor responses.
     
     % Number of scales
     % TODO this is super sloppy..
@@ -10,83 +9,83 @@ function curv_final = NCZLd_channel_ON_OFF_v1_1(curv, config)
     % choose the algorithm (separated, abs, quadratic) 
     switch config.zli.ON_OFF
         case 0 % Separated
-            curv_final = process_ON_OFF_separately(curv, config);
+            Iitheta_final = process_ON_OFF_separately(Iitheta, config);
         case 1 % ABS
-            curv_final = process_ON_OFF_abs(curv, config);
+            Iitheta_final = process_ON_OFF_abs(Iitheta, config);
         case 2 % Square (quadratic)
-            curv_final = process_ON_OFF_square(curv, config);
+            Iitheta_final = process_ON_OFF_square(Iitheta, config);
     end
 end
 
-function curv_final = process_ON_OFF_separately(curv, config)
+function Iitheta_final = process_ON_OFF_separately(Iitheta, config)
 %PROCESS_ON_OFF_SEPARATELY Process the ON and OFF channels independently.
 
     n_membr  = config.zli.n_membr;
     
     %% Initialize data structures
-    curv_ON        = curv;
-    curv_OFF       = curv;
-    curv_ON_final  = curv;
-    curv_OFF_final = curv;
-    curv_final     = cell(size(curv));
+    Iitheta_ON        = Iitheta;
+    Iitheta_OFF       = Iitheta;
+    Iitheta_ON_final  = Iitheta;
+    Iitheta_OFF_final = Iitheta;
+    Iitheta_final     = cell(size(Iitheta));
 
     %% Calculate the ON/OFF signals
     for t=1:n_membr
-        index_OFF             = curv{t} <= 0;
-        index_ON              = curv{t} >= 0;
-        curv_ON{t}            =  curv{t};
-        curv_OFF{t}           = -curv{t};
-        curv_ON{t}(index_OFF) = 0;
-        curv_OFF{t}(index_ON) = 0;
+        index_OFF                = Iitheta{t} <= 0;
+        index_ON                 = Iitheta{t} >= 0;
+        Iitheta_ON{t}            =  Iitheta{t};
+        Iitheta_OFF{t}           = -Iitheta{t};
+        Iitheta_ON{t}(index_OFF) = 0;
+        Iitheta_OFF{t}(index_ON) = 0;
     end
 
     %% Positius +++++++++++++++++++++++++++++++++++++++++++++++++++
     disp('Starting ON processing');
     % TODO we should have an iFactor for each dimension!!
-    iFactor_ON  = model.process.Rmodelinductiond_v0_3_2(curv_ON, config);
+    iFactor_ON  = model.process.Rmodelinductiond_v0_3_2(Iitheta_ON, config);
 
     %% Negatius ----------------------------------------------------
     disp('Starting OFF processing');
-    iFactor_OFF = model.process.Rmodelinductiond_v0_3_2(curv_OFF, config);
+    iFactor_OFF = model.process.Rmodelinductiond_v0_3_2(Iitheta_OFF, config);
 
     %% Prepare output
     iFactor = iFactor_ON;
     for t=1:n_membr
-        curv_ON_final{t}  =  curv_ON{t}     .* iFactor_ON{t}  * config.zli.normal_output;
-        curv_OFF_final{t} = -curv_OFF{t}    .* iFactor_OFF{t} * config.zli.normal_output;
-        iFactor{t}        = iFactor_ON{t}    + iFactor_OFF{t};
-        curv_final{t}     = curv_ON_final{t} + curv_OFF_final{t};
+        Iitheta_ON_final{t}  =  Iitheta_ON{t}     .* iFactor_ON{t}  * config.zli.normal_output;
+        Iitheta_OFF_final{t} = -Iitheta_OFF{t}    .* iFactor_OFF{t} * config.zli.normal_output;
+        iFactor{t}           = iFactor_ON{t}       + iFactor_OFF{t};
+        Iitheta_final{t}     = Iitheta_ON_final{t} + Iitheta_OFF_final{t};
     end
 end
 
-function [curv_final] = process_ON_OFF_abs(curv, config)
+function [Iitheta_final] = process_ON_OFF_abs(Iitheta, config)
 %PROCESS_ON_OFF_ABS Process the ON and OFF channels using absolute values.
 %   Note: This has not been updated since Sean's refactoring of the data
 %         structure. Anyway, from looking at the code, it never worked in
 %         the first place; it doesn't return the ON/OFF data.
-    dades = curv;
+    dades = Iitheta;
     for t=1:n_membr
-        dades{t} = abs(curv{t});
+        dades{t} = abs(Iitheta{t});
     end
 
     iFactor = Rmodelinductiond_v0_3_2(dades, config);
 
     for t=1:n_membr
-        curv_final{t} = curv{t} .* iFactor{t} * zli.normal_output;
+        Iitheta_final{t} = Iitheta{t} .* iFactor{t} * zli.normal_output;
     end
 end
 
-function [curv_final] = process_ON_OFF_square(curv, config)
+function [Iitheta_final] = process_ON_OFF_square(Iitheta, config)
 %PROCESS_ON_OFF_SQUARE Process the ON and OFF channels using square
 %   Note: This has not been updated since Sean's refactoring of the data
 %         structure. Anyway, from looking at the code, it never worked in
 %         the first place; it doesn't return the ON/OFF data.
-    dades = curv;
+    dades = Iitheta;
     for t=1:n_membr
-        dades{t} = curv{t}.*curv{t};
+        dades{t} = Iitheta{t}.*Iitheta{t};
     end
     iFactor = Rmodelinductiond_v0_3_2(dades, config);
     for t=1:n_membr
-        curv_final{t} = curv{t} .* iFactor{t} * zli.normal_output;
+        Iitheta_final{t} = Iitheta{t} .* iFactor{t} * zli.normal_output;
     end
 end
