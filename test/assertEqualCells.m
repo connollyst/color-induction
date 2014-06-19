03068f32c61e0ac79075d9cdb0ca995804f8c9da
Sean Connolly <connolly.st@gmail.com>
2014-06-19 23:22:11 +0200

    Added better output for assertion

--- test/assertEqualCells.m
+++ test/assertEqualCells.m
@@ -1,5 +1,5 @@
 function assertEqualCells(actual, expected)
-    assertEqual(size(actual), size(expected));
+    assertEqual(size(actual), size(expected), 'cell arrays should be the same size');
     for i=1:length(actual)
         assertEqualMatrices(actual{i}, expected{i})
     end
