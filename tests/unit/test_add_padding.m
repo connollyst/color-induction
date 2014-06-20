ae518d51c5fe3dcab739c70b29862ca45020122c
Sean Connolly <connolly.st@gmail.com>
2014-06-20 17:21:06 +0200

    Updated add_padding() tests with new data struct

--- tests/unit/test_add_padding.m
+++ tests/unit/test_add_padding.m
@@ -3,38 +3,69 @@ function test_suite = test_add_padding
   initTestSuite;
 end
 
-function test_padded_newgx_toroidal_x_01
-    assert_padded_newgx_toroidal_x('01');
+function test_padded_newgx_toroidal_x_3D_t01_i01
+    assert_padded_newgx_toroidal_x('3D_t01_i01');
 end
 
-function test_padded_newgy_toroidal_y_01
-    assert_padded_newgy_toroidal_y('01');
+function test_padded_newgy_toroidal_y_3D_t01_i01
+    assert_padded_newgy_toroidal_y('3D_t01_i01');
 end
 
-function test_padded_restr_newgx_toroidal_x_01
-    assert_padded_restr_newgx_toroidal_x('01')
+function test_padded_restr_newgx_toroidal_x_3D_t01_i01
+    assert_padded_restr_newgx_toroidal_x('3D_t01_i01')
 end
 
-function test_padded_restr_newgy_toroidal_y_01
-    assert_padded_restr_newgy_toroidal_y('01')
+function test_padded_restr_newgy_toroidal_y_3D_t01_i01
+    assert_padded_restr_newgy_toroidal_y('3D_t01_i01')
 end
 
-function test_padded_newgx_toroidal_x_02
-    assert_padded_newgx_toroidal_x('02');
+function test_padded_newgx_toroidal_x_3D_t01_i02
+    assert_padded_newgx_toroidal_x('3D_t01_i02');
 end
 
-function test_padded_newgy_toroidal_y_02
-    assert_padded_newgy_toroidal_y('02');
+function test_padded_newgy_toroidal_y_3D_t01_i02
+    assert_padded_newgy_toroidal_y('3D_t01_i02');
 end
 
-function test_padded_restr_newgx_toroidal_x_02
-    assert_padded_restr_newgx_toroidal_x('02')
+function test_padded_restr_newgx_toroidal_x_3D_t01_i02
+    assert_padded_restr_newgx_toroidal_x('3D_t01_i02')
 end
 
-function test_padded_restr_newgy_toroidal_y_02
-    assert_padded_restr_newgy_toroidal_y('02')
+function test_padded_restr_newgy_toroidal_y_3D_t01_i02
+    assert_padded_restr_newgy_toroidal_y('3D_t01_i02')
 end
 
+function test_padded_newgx_toroidal_x_3D_t01_i03
+    assert_padded_newgx_toroidal_x('3D_t01_i03');
+end
+
+function test_padded_newgy_toroidal_y_3D_t01_i03
+    assert_padded_newgy_toroidal_y('3D_t01_i03');
+end
+
+function test_padded_restr_newgx_toroidal_x_3D_t01_i03
+    assert_padded_restr_newgx_toroidal_x('3D_t01_i03')
+end
+
+function test_padded_restr_newgy_toroidal_y_3D_t01_i03
+    assert_padded_restr_newgy_toroidal_y('3D_t01_i03')
+end
+
+function test_padded_newgx_toroidal_x_3D_t04_i01
+    assert_padded_newgx_toroidal_x('3D_t04_i01');
+end
+
+function test_padded_newgy_toroidal_y_3D_t04_i01
+    assert_padded_newgy_toroidal_y('3D_t04_i01');
+end
+
+function test_padded_restr_newgx_toroidal_x_3D_t04_i01
+    assert_padded_restr_newgx_toroidal_x('3D_t04_i01')
+end
+
+function test_padded_restr_newgy_toroidal_y_3D_t04_i01
+    assert_padded_restr_newgy_toroidal_y('3D_t04_i01')
+end
 %% ASSERTIONS
 
 function assert_padded_newgx_toroidal_x(instance)
@@ -55,28 +86,27 @@ function assert_padded_restr_newgx_toroidal_x(instance)
     [x, y, Delta, interactions, config] = get_input(instance);
     [~, ~, restr_newgx_toroidal_x, ~] = model.add_padding(x, y, Delta, interactions, config);
     expected = get_output(instance);
-    assertEqualMatrices(restr_newgx_toroidal_x, expected.restr_newgx_toroidal_x);
+    assertEqual(restr_newgx_toroidal_x, expected.restr_newgx_toroidal_x);
 end
 
 function assert_padded_restr_newgy_toroidal_y(instance)
     [x, y, Delta, interactions, config] = get_input(instance);
     [~, ~, ~, restr_newgy_toroidal_y] = model.add_padding(x, y, Delta, interactions, config);
     expected = get_output(instance);
-    assertEqualMatrices(restr_newgy_toroidal_y, expected.restr_newgy_toroidal_y);
+    assertEqual(restr_newgy_toroidal_y, expected.restr_newgy_toroidal_y);
 end
 
 %% TEST UTILITIES
 
 function [x, y, Delta, interactions, config] = get_input(instance)
-    input        = load(['data/input_to_add_padding_',instance,'.mat']);
-    x            = addColorDimension(input.x);
-    y            = addColorDimension(input.y);
+    input        = load(['data/input/add_padding_',instance,'.mat']);
+    x            = input.x;
+    y            = input.y;
     Delta        = input.Delta;
-    state        = load('data/state_UpdateXY.mat');
-    interactions = state.interactions;
-    config       = state.config;
+    interactions = input.interactions;
+    config       = input.config;
 end
 
 function output = get_output(instance)
-    output = load(['data/expected_from_add_padding_',instance,'.mat']);
+    output = load(['data/expected/add_padding_',instance,'.mat']);
 end
