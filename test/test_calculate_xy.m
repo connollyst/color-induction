6f0ec528bd8c0ae7c45add3c84bc37a2ef548548
Sean Connolly <connolly.st@gmail.com>
2014-06-20 11:35:49 +0200

    Added tests for calculate_xy() in 3D

--- test/test_calculate_xy.m
+++ test/test_calculate_xy.m
@@ -11,6 +11,60 @@ function test_y_out_01
     assert_y_out('01')
 end
 
+function test_x_out_3D_01
+    % Get input
+    input        = load('data/input_to_calculate_xy_3D_01.mat');
+    tIitheta     = input.tIitheta;
+    I_norm       = input.I_norm;
+    x            = input.x;
+    y            = input.y;
+    x_ee         = input.x_ee;
+    x_ei         = input.x_ei;
+    y_ie         = input.y_ie;
+    config       = input.config;
+    % Get expected results
+    expected = get_expected('3D_t01_i01');
+    % Run UpdateXY
+    [x_out, ~] = model.calculate_xy(tIitheta, I_norm, x, y, x_ee, x_ei, y_ie, config);
+    % Compare results
+    for c=1:size(x_out, 3)
+        for s=1:size(x_out, 4)
+            for o=1:size(x_out, 5)
+                assertEqual(x_out(:,:,c,s,o), expected.x(:,:,c,s,o), ...
+                    ['Matrices not equals at c=',num2str(c),' s=',num2str(s),' o=',num2str(o),' (at least)'] ...
+                );
+            end
+        end
+    end
+end
+
+function test_y_out_3D_01
+    % Get input
+    input        = load('data/input_to_calculate_xy_3D_01.mat');
+    tIitheta     = input.tIitheta;
+    I_norm       = input.I_norm;
+    x            = input.x;
+    y            = input.y;
+    x_ee         = input.x_ee;
+    x_ei         = input.x_ei;
+    y_ie         = input.y_ie;
+    config       = input.config;
+    % Get expected results
+    expected = get_expected('3D_t01_i01');
+    % Run UpdateXY
+    [~, y_out] = model.calculate_xy(tIitheta, I_norm, x, y, x_ee, x_ei, y_ie, config);
+    % Compare results
+    for c=1:size(y_out, 3)
+        for s=1:size(y_out, 4)
+            for o=1:size(y_out, 5)
+                assertEqual(y_out(:,:,c,s,o), expected.y(:,:,c,s,o), ...
+                    ['Matrices not equals at c=',num2str(c),' s=',num2str(s),' o=',num2str(o),' (at least)'] ...
+                );
+            end
+        end
+    end
+end
+
 %% ASSERTIONS
 
 function assert_x_out(instance)
