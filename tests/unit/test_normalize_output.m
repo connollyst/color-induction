a18308615c62548bf2372bc122af2a7accbae252
Sean Connolly <connolly.st@gmail.com>
2014-06-20 17:33:32 +0200

    Updated normalize_output() test data

--- tests/unit/test_normalize_output.m
+++ tests/unit/test_normalize_output.m
@@ -3,16 +3,20 @@ function test_suite = test_normalize_output
   initTestSuite;
 end
 
-function test_I_norm_01
-    assert_I_norm('01')
+function test_I_norm_3D_t01_i01
+    assert_I_norm('3D_t01_i01')
 end
 
-function test_I_norm_02
-    assert_I_norm('02')
+function test_I_norm_3D_t01_i02
+    assert_I_norm('3D_t01_i02')
 end
 
-function test_I_norm_09
-    assert_I_norm('09')
+function test_I_norm_3D_t01_i03
+    assert_I_norm('3D_t01_i03')
+end
+
+function test_I_norm_3D_t04_i01
+    assert_I_norm('3D_t04_i01')
 end
 
 %% ASSERTIONS
@@ -22,20 +26,19 @@ function assert_I_norm(instance)
     [norm_mask, newgx_toroidal_x, interactions, config] = get_input(instance);
     I_norm   = model.normalize_output(norm_mask, newgx_toroidal_x, interactions, config);
     expected = get_expected(instance);
-    assertEqualMatrices(I_norm, expected.I_norm);
+    assertEqual(I_norm, expected.I_norm);
 end
 
 %% TEST UTILITIES
 
 function [norm_mask, newgx_toroidal_x, interactions, config] = get_input(instance)
-    input            = load(['data/input_to_normalize_output_',instance,'.mat']);
-    newgx_toroidal_x = addColorDimension(input.newgx_toroidal_x);
-    state            = load('data/state_UpdateXY.mat');
-    norm_mask        = state.norm_mask;
-    interactions     = state.interactions;
-    config           = state.config;
+    input            = load(['data/input/normalize_output_',instance,'.mat']);
+    newgx_toroidal_x = input.newgx_toroidal_x;
+    norm_mask        = input.norm_mask;
+    interactions     = input.interactions;
+    config           = input.config;
 end
 
 function expected = get_expected(instance)
-    expected = load(['data/expected_from_normalize_output_',instance,'.mat']);
+    expected = load(['data/expected/normalize_output_',instance,'.mat']);
 end
