28a1b9bb652e30dea44e962b4060237ac2bfab3a
Sean Connolly <connolly.st@gmail.com>
2014-06-29 15:38:40 +0200

    IMPORTANT: optima_fft now uses ifftn(), not ifft()
    This appears to have been a bug in the original PLoS One code. The
    result of the inverse FFT does not return information in the ‘real’
    data space, but in the Fourier space still.

--- +convolutions/optima_fft.m
+++ +convolutions/optima_fft.m
@@ -1,10 +1,16 @@
-function res = optima_fft(data_fft, filter_fft, half_size_filter, avoid_circshift_fft)
-%  Performs discrete (using convn) or FFT (fftfilt) convolution depending
-%  on data and filter sizes
+function res = optima_fft(data_fft, filter_fft, shift_size, avoid_circshift_fft)
+%OPTIMA_FFT Apply the Fourier filter to the Fourier input data.
+%   The Fourier filter is applied to the Fourier data and then the inverse
+%   Fourier transform is applied to return the result.
+%
+%       data_fft:   the data to be filtered, already in Fourier space
+%       filter_fft: the filer to be applied, already in Fourier space
+%       shift_size: the shift size filter if avoiding FFT circshift
+%       avoid_circshift_fft: 0/1 if we should compensate for FFT circhift
+
     conv_fft = data_fft.*filter_fft;
-    % TODO should we NOT be using ifftn here?
-    res = ifft(conv_fft, 'symmetric');
+    res = ifftn(conv_fft, 'symmetric');
     if avoid_circshift_fft ~= 1
-        res = circshift(res, -half_size_filter);
+        res = circshift(res, -shift_size);
     end
 end
