6da03c67126d244976ce091fdd98cd22427e92dd
Sean Connolly <connolly.st@gmail.com>
2014-06-29 20:38:46 +0200

    More explicit output in tests

--- tests/assertEqualData.m
+++ tests/assertEqualData.m
@@ -9,7 +9,17 @@ end
 function assertEqualDataCells(actual, expected)
     assertEqual(size(actual), size(expected), 'cell arrays should be the same size');
     for t=1:length(actual)
-        assertEqualDataMatrices(actual{t}, expected{t})
+        for c=1:size(actual, 3)
+        for s=1:size(actual, 4)
+            for o=1:size(actual, 5)
+                nextActual   = actual(:,:,c,s,o);
+                nextExpected = expected(:,:,c,s,o);
+                assertEqual(nextActual, nextExpected, ...
+                    ['Expected same matrix at t=',num2str(t),', c=',num2str(c),', s=',num2str(s),', o=',num2str(o)] ...
+                );
+            end
+        end
+    end
     end
 end
 
