b5de3f4ffb2969e4982de056988457127eb35898
Sean Connolly <connolly.st@gmail.com>
2014-07-09 18:57:00 +0200

    Removed duplication

--- +model/+terms/x_ee_y_ie.m
+++ +model/+terms/x_ee_y_ie.m
@@ -17,13 +17,13 @@ function [x_ee_conv_tmp, y_ie_conv_tmp] = x_ee_y_ie(oc, gx_padded, JW, interacti
     x_ee_conv_tmp = zeros(n_cols, n_rows, n_channels, n_scales, n_orients);
     y_ie_conv_tmp = zeros(n_cols, n_rows, n_channels, n_scales, n_orients);
     for ov=1:n_orients  % loop over all the orientations given the central (reference orientation)
-        if use_fft
-            for s=1:n_scales
-                cols     = scale_deltas(s)+1:scale_deltas(s)+n_cols;
-                rows     = scale_deltas(s)+1:scale_deltas(s)+n_rows;
-                J_fft_s  = JW.J_fft{s}(:,:,1,ov,oc);
-                W_fft_s  = JW.W_fft{s}(:,:,1,ov,oc);
-                s_filter = half_size_filter{s};
+        for s=1:n_scales
+            cols     = scale_deltas(s)+1:scale_deltas(s)+n_cols;
+            rows     = scale_deltas(s)+1:scale_deltas(s)+n_rows;
+            J_fft_s  = JW.J_fft{s}(:,:,1,ov,oc);
+            W_fft_s  = JW.W_fft{s}(:,:,1,ov,oc);
+            s_filter = half_size_filter{s};
+            if use_fft
                 for c=1:n_channels
                     x_fft    = gx_padded{scale_distance+s,c}{ov};
                     x_fft_J  = convolutions.optima_fft(x_fft, J_fft_s, s_filter, avoid_circshift_fft);
@@ -31,15 +31,9 @@ function [x_ee_conv_tmp, y_ie_conv_tmp] = x_ee_y_ie(oc, gx_padded, JW, interacti
                     x_ee_conv_tmp(:,:,c,s,ov) = x_fft_J(cols, rows);
                     y_ie_conv_tmp(:,:,c,s,ov) = x_fft_W(cols, rows);
                 end
-            end
-        else
-            for s=1:n_scales
-                cols     = scale_deltas(s)+1:scale_deltas(s)+n_cols;
-                rows     = scale_deltas(s)+1:scale_deltas(s)+n_rows;
-                J_fft_s  = JW.J_fft{s}(:,:,1,ov,oc);
-                W_fft_s  = JW.W_fft{s}(:,:,1,ov,oc);
-                s_filter = half_size_filter{s};
+            else
                 for c=1:n_channels
+                    % TODO why is gx_padded size not the same in FFT?
                     x    = gx_padded{scale_distance+s}(:,:,c,ov);
                     x_J  = convolutions.optima(x, J_fft_s, s_filter, 1, avoid_circshift_fft);
                     x_W  = convolutions.optima(x, W_fft_s, s_filter, 1, avoid_circshift_fft);
