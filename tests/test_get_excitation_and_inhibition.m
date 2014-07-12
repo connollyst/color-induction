2ca14f2203988b1c7c2a5d33cc506e54dd86697b
Sean Connolly <connolly.st@gmail.com>
2014-07-12 20:07:36 +0200

    Disabled color interaction in tests

--- tests/test_get_excitation_and_inhibition.m
+++ tests/test_get_excitation_and_inhibition.m
@@ -4,111 +4,111 @@ function test_suite = test_get_excitation_and_inhibition
 end
 
 function test_x_ee_3D_t01_i01
-    assert_x_ee('3D_t01_i01', 0);
+    assert_x_ee('3D_t01_i01', 0, 0);
 end
 function test_x_ee_3D_t01_i01_fft
-    assert_x_ee('3D_t01_i01', 1);
+    assert_x_ee('3D_t01_i01', 1, 0);
 end
 
 function test_x_ei_3D_t01_i01
-    assert_x_ei('3D_t01_i01', 0);
+    assert_x_ei('3D_t01_i01', 0, 0);
 end
 function test_x_ei_3D_t01_i01_fft
-    assert_x_ei('3D_t01_i01', 1);
+    assert_x_ei('3D_t01_i01', 1, 0);
 end
 
 function test_y_ie_3D_t01_i01
-    assert_y_ie('3D_t01_i01', 0);
+    assert_y_ie('3D_t01_i01', 0, 0);
 end
 function test_y_ie_3D_t01_i01_fft
-    assert_y_ie('3D_t01_i01', 1);
+    assert_y_ie('3D_t01_i01', 1, 0);
 end
 
 function test_x_ee_3D_t01_i02
-    assert_x_ee('3D_t01_i02', 0);
+    assert_x_ee('3D_t01_i02', 0, 0);
 end
 function test_x_ee_3D_t01_i02_fft
-    assert_x_ee('3D_t01_i02', 1);
+    assert_x_ee('3D_t01_i02', 1, 0);
 end
 
 function test_x_ei_3D_t01_i02
-    assert_x_ei('3D_t01_i02', 0);
+    assert_x_ei('3D_t01_i02', 0, 0);
 end
 function test_x_ei_3D_t01_i02_fft
-    assert_x_ei('3D_t01_i02', 1);
+    assert_x_ei('3D_t01_i02', 1, 0);
 end
 
 function test_y_ie_3D_t01_i02
-    assert_y_ie('3D_t01_i02', 0);
+    assert_y_ie('3D_t01_i02', 0, 0);
 end
 function test_y_ie_3D_t01_i02_fft
-    assert_y_ie('3D_t01_i02', 1);
+    assert_y_ie('3D_t01_i02', 1, 0);
 end
 
 function test_x_ee_3D_t01_i03
-    assert_x_ee('3D_t01_i03', 0);
+    assert_x_ee('3D_t01_i03', 0, 0);
 end
 function test_x_ee_3D_t01_i03_fft
-    assert_x_ee('3D_t01_i03', 1);
+    assert_x_ee('3D_t01_i03', 1, 0);
 end
 
 function test_x_ei_3D_t01_i03
-    assert_x_ei('3D_t01_i03', 0);
+    assert_x_ei('3D_t01_i03', 0, 0);
 end
 function test_x_ei_3D_t01_i03_fft
-    assert_x_ei('3D_t01_i03', 1);
+    assert_x_ei('3D_t01_i03', 1, 0);
 end
 
 function test_y_ie_3D_t01_i03
-    assert_y_ie('3D_t01_i03', 0);
+    assert_y_ie('3D_t01_i03', 0, 0);
 end
 function test_y_ie_3D_t01_i03_fft
-    assert_y_ie('3D_t01_i03', 1);
+    assert_y_ie('3D_t01_i03', 1, 0);
 end
 
 function test_x_ee_3D_t04_i01
-    assert_x_ee('3D_t04_i01', 0);
+    assert_x_ee('3D_t04_i01', 0, 0);
 end
 function test_x_ee_3D_t04_i01_fft
-    assert_x_ee('3D_t04_i01', 1);
+    assert_x_ee('3D_t04_i01', 1, 0);
 end
 
 function test_x_ei_3D_t04_i01
-    assert_x_ei('3D_t04_i01', 0);
+    assert_x_ei('3D_t04_i01', 0, 0);
 end
 function test_x_ei_3D_t04_i01_fft
-    assert_x_ei('3D_t04_i01', 1);
+    assert_x_ei('3D_t04_i01', 1, 0);
 end
 
 function test_y_ie_3D_t04_i01
-    assert_y_ie('3D_t04_i01', 0);
+    assert_y_ie('3D_t04_i01', 0, 0);
 end
 function test_y_ie_3D_t04_i01_fft
-    assert_y_ie('3D_t04_i01', 1);
+    assert_y_ie('3D_t04_i01', 1, 0);
 end
 
 %% ASSERTIONS
 
 % Assert that, given known input, the output is as expected
 
-function assert_x_ee(instance, use_fft)
-    config = get_config(use_fft);
+function assert_x_ee(instance, use_fft, channel_interaction)
+    config = get_config(use_fft, channel_interaction);
     [gx_padded, gy_padded, JW, interactions] = get_input(instance);
     [x_ee, ~, ~] = model.get_excitation_and_inhibition(gx_padded, gy_padded, JW, interactions, config);
     expected = get_expected(instance);
     assertEqualData(x_ee, expected.x_ee);
 end
 
-function assert_x_ei(instance, use_fft)
-    config = get_config(use_fft);
+function assert_x_ei(instance, use_fft, channel_interaction)
+    config = get_config(use_fft, channel_interaction);
     [gx_padded, gy_padded, JW, interactions] = get_input(instance);
     [~, x_ei, ~] = model.get_excitation_and_inhibition(gx_padded, gy_padded, JW, interactions, config);
     expected = get_expected(instance);
     assertEqualData(x_ei, expected.x_ei);
 end
 
-function assert_y_ie(instance, use_fft)
-    config = get_config(use_fft);
+function assert_y_ie(instance, use_fft, channel_interaction)
+    config = get_config(use_fft, channel_interaction);
     [gx_padded, gy_padded, JW, interactions] = get_input(instance);
     [~, ~, y_ie] = model.get_excitation_and_inhibition(gx_padded, gy_padded, JW, interactions, config);
     expected = get_expected(instance);
@@ -117,10 +117,11 @@ end
 
 %% TEST UTILITIES
 
-function config = get_config(use_fft)
+function config = get_config(use_fft, channel_interaction)
     saved = load('data/input/config_40x40x3.mat');
     config = saved.config;
     config.compute.use_fft = use_fft;
+    config.zli.channel_interaction = channel_interaction;
 end
 
 function [gx_padded, gy_padded, JW, interactions] = get_input(instance)
