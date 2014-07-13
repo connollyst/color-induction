c2fbc720846613ef0142e2b6fcb6544e163c8851
Sean Connolly <connolly.st@gmail.com>
2014-07-13 15:10:42 +0200

    Renamed return variables for readability

--- +model/+wavelet/+functions/a_trous.m
+++ +model/+wavelet/+functions/a_trous.m
@@ -1,13 +1,13 @@
-function [w, c] = a_trous(image, scales)
+function [wavelets, residuals] = a_trous(image, scales)
 % Implementation of A Trous wavelet transform
 %
 % inputs:
-%   image: input image to be decomposed.
+%   image:  input image to be decomposed.
 %   scales: # of wavelet levels.
 %
 % outputs:
-%   w: cell array of wavelet planes.
-%   c: cell array of residual planes.
+%   wavelets:  cell array of wavelet planes.
+%   residuals: cell array of residual planes.
 
 	h = [1.0/256.0, 1.0/64.0, 3.0/128.0, 1.0/64.0, 1.0/256.0;
          1.0/64.0,  1.0/16.0, 3.0/32.0,  1.0/16.0, 1.0/64.0;
@@ -18,17 +18,16 @@ function [w, c] = a_trous(image, scales)
 	energy     = sum(h(:));
 	inv_energy = 1/energy;
 	h          = h*inv_energy;
-	w          = cell(scales, 1);
-	c          = cell(scales, 1);
+	wavelets   = cell(scales, 1);
+	residuals  = cell(scales, 1);
     
     for s=1:scales
-		inv_energy = 1/sum(h(:));
-		prod   = model.wavelet.functions.utils.symmetric_filtering(image, h) * inv_energy;  % blur
-		DF     = image - prod;                                                              % wavelet plane
-		w{s,1} = DF;
-		c{s,1} = prod;                                                                      % residual
-		image  = c{s,1};
-		h      = padarray(upsample(upsample(h,2)',2),[1 1],0,'pre')';                       % upsample filter
+		inv_energy     = 1/sum(h(:));
+		prod           = model.wavelet.functions.utils.symmetric_filtering(image, h) * inv_energy;  % blur
+		wavelets{s,1}  = image - prod;
+		residuals{s,1} = prod;
+		image          = residuals{s,1};
+		h              = padarray(upsample(upsample(h,2)',2),[1 1],0,'pre')';                       % upsample filter
     end
 end
 
