c8f0518c1472f21b6a5aeff17addd2bfaa893734
Sean Connolly <connolly.st@gmail.com>
2014-07-13 22:11:49 +0200

    Simplified non-directional J&W fft

--- +model/+terms/+interactions/+jw/nondirectional.m
+++ +model/+terms/+interactions/+jw/nondirectional.m
@@ -11,7 +11,6 @@ function JW = nondirectional(interactions, config)
     n_cols     = config.image.width;
     n_rows     = config.image.height;
     n_scales   = config.wave.n_scales;
-    n_orients  = config.wave.n_orients;
     diameter   = 2*scale_deltas+1;  % maximum diameter of the area of influence
     all_J      = cell(n_scales, 1);
     all_W      = cell(n_scales, 1);
@@ -31,37 +30,25 @@ function JW = nondirectional(interactions, config)
     end
 
     for s=1:n_scales
-        if scale_distance > 0
-            % Why doe we have a scale dimension of n=1??
-            J = zeros(diameter(s), diameter(s), 1, n_orients, n_orients);
-            W = zeros(diameter(s), diameter(s), 1, n_orients, n_orients);
-            J(:,:,1,:,:) = 1 * all_J{s}(:,:,:,:);
-            W(:,:,1,:,:) = 1 * all_W{s}(:,:,:,:);
-            all_J{s} = J;
-            all_W{s} = W;
-        end
+        diameter_s    = diameter(s);
+        scale_delta_s = scale_deltas(s);
+        scale_cols    = n_cols + 2 * scale_delta_s;
+        scale_rows    = n_rows + 2 * scale_delta_s;
         % a matrix for each scale
         % fft for speed (convolutions are computed in another space)
-        all_J_fft{s} = zeros(n_cols+2*scale_deltas(s), n_rows+2*scale_deltas(s), 1, n_orients, n_orients);
-        all_W_fft{s} = zeros(n_cols+2*scale_deltas(s), n_rows+2*scale_deltas(s), 1, n_orients, n_orients);
-        all_W_fft{s} = zeros(n_cols+2*scale_deltas(s), n_rows+2*scale_deltas(s));
-        for ov=1:n_orients
-            for oc=1:n_orients
-                if config.compute.avoid_circshift_fft==1
-                    % FFT that does not require circshift (by far better)
-                    padsize = [n_cols+2*scale_deltas(s)-diameter(s),n_rows+2*scale_deltas(s)-diameter(s)];
-                    J_circ = padarray(all_J{s}(:,:,1,ov,oc), padsize, 0,'post');
-                    W_circ = padarray(all_W{s}(:,:,1,ov,oc), padsize, 0,'post');
-                    J_circ = circshift(J_circ, -[scale_deltas(s) scale_deltas(s)]);
-                    W_circ = circshift(W_circ, -[scale_deltas(s) scale_deltas(s)]);
-                    all_J_fft{s}(:,:,1,ov,oc) = fftn(J_circ);
-                    all_W_fft{s}(:,:,1,ov,oc) = fftn(W_circ);
-                else
-                    % FFT that requires circshift
-                    all_J_fft{s}(:,:,1,ov,oc) = fftn(all_J{s}(:,:,1,ov,oc),[n_cols+2*scale_deltas(s),n_rows+2*scale_deltas(s)]);
-                    all_W_fft{s}(:,:,1,ov,oc) = fftn(all_W{s}(:,:,1,ov,oc),[n_cols+2*scale_deltas(s),n_rows+2*scale_deltas(s)]);
-                end
-            end
+        if config.compute.avoid_circshift_fft==1
+            % FFT that does not require circshift (by far better)
+            padsize      = [scale_cols-diameter_s, scale_rows-diameter_s];
+            J_circ       = padarray(all_J{s}, padsize, 0, 'post');
+            W_circ       = padarray(all_W{s}, padsize, 0, 'post');
+            J_circ       = circshift(J_circ, -[scale_delta_s scale_delta_s]);
+            W_circ       = circshift(W_circ, -[scale_delta_s scale_delta_s]);
+            all_J_fft{s} = fftn(J_circ);
+            all_W_fft{s} = fftn(W_circ);
+        else
+            % FFT that requires circshift
+            all_J_fft{s} = fftn(all_J{s}, [scale_cols, scale_rows]);
+            all_W_fft{s} = fftn(all_W{s}, [scale_cols, scale_rows]);
         end
     end
     
@@ -70,6 +57,4 @@ function JW = nondirectional(interactions, config)
     JW.W = all_W;
     JW.J_fft = all_J_fft;
     JW.W_fft = all_W_fft;
-    
-    error('Not yet implemented')
 end
