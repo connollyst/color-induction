e67f02ff339081643b38b4f07c9f2d1060be69d9
Sean Connolly <connolly.st@gmail.com>
2014-07-16 17:18:12 +0200

    Enabled color interactions in double_opponent cfg

--- +configurations/double_opponent.m
+++ +configurations/double_opponent.m
@@ -2,4 +2,5 @@ function config = double_opponent()
     config = configurations.default();
     config.wave.transform = 'DWD_orient_undecimated';
     config.wave.n_orients = 3;
+    config.zli.channel_interactions = 1;
 end
