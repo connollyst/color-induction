f2bf369cc677362b9b7981f16371f7199f8e1b46
Sean Connolly <connolly.st@gmail.com>
2014-07-17 18:04:33 +0200

    Corrected double opponent config

--- +configurations/double_opponent.m
+++ +configurations/double_opponent.m
@@ -5,5 +5,5 @@ function config = double_opponent()
     config.wave.n_orients = 3;
     % Treat the ON & OFF values as interacting opponents
     config.zli.ON_OFF = 'opponent';
-    config.zli.channel_interactions = 1;
+    config.zli.channel_interaction = 1;
 end
