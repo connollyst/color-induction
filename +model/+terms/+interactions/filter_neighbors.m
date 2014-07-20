e2b96e402b7b7c27cc795704ae0e19287266c4ae
Sean Connolly <connolly.st@gmail.com>
2014-07-20 19:03:00 +0200

    Corrected shape of color filter

--- +model/+terms/+interactions/filter_neighbors.m
+++ +model/+terms/+interactions/filter_neighbors.m
@@ -4,6 +4,6 @@ function neigbor_filter = filter_neighbors(a, b)
 % The influence from other channels is mediated by the configured weight.
     
     % TODO this is hard coded for color!! can be reused for scales.
-    neigbor_filter = zeros(1, 1, 1, 3, 1);
-    neigbor_filter(1, 1, 1, :, 1) = [b a b];
+    neigbor_filter = zeros(1, 1, 3, 1, 1);
+    neigbor_filter(1, 1, :, 1, 1) = [b a b];
 end
