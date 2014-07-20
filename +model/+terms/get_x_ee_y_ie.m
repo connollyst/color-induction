bd8c0ab9f2232d81edaea99c6186315eb2aa35be
Sean Connolly <connolly.st@gmail.com>
2014-07-20 19:55:14 +0200

    Added support for disabling scale interactions

--- +model/+terms/get_x_ee_y_ie.m
+++ +model/+terms/get_x_ee_y_ie.m
@@ -121,8 +121,12 @@ end
 
 function interaction = apply_scale_interaction(data, scale_interactions)
 % Apply scale filter to get interactions between scales.
-    scale_filter = scale_interactions.filter;
-    interaction  = model.data.convolutions.optima(data, scale_filter, 0, 0);
+    if ~config.zli.interaction.scale.enabled
+        interaction  = data;
+    else
+        scale_filter = scale_interactions.filter;
+        interaction  = model.data.convolutions.optima(data, scale_filter, 0, 0);
+    end
 end
 
 function gx_padded_fft = apply_fft(gx_padded)
