85c8c6eea92cf200a706e61a31d8bc36cc1eb2e0
Sean Connolly <connolly.st@gmail.com>
2014-07-20 19:03:15 +0200

    Opponent color filter tests

--- tests/+unit_tests/+model/+terms/+interactions/+colors/test_excitation_filter.m
+++ tests/+unit_tests/+model/+terms/+interactions/+colors/test_excitation_filter.m
@@ -22,7 +22,7 @@ function test_default_color_excitation_filter_with_1_color_channel
     config = struct();
     config.zli.interaction.color.enabled = true;
     config.zli.interaction.color.scheme  = 'default';
-    config.zli.interaction.color.weight  = 0.1;
+    config.zli.interaction.color.weight  = 0.3;
     config.image.n_channels              = 1;
     % When
     actual_filter = model.terms.interactions.colors.excitation_filter(config);
@@ -36,15 +36,15 @@ function test_default_color_excitation_filter_with_2_color_channel
     config = struct();
     config.zli.interaction.color.enabled = true;
     config.zli.interaction.color.scheme  = 'default';
-    config.zli.interaction.color.weight  = 0.1;
+    config.zli.interaction.color.weight  = 0.2;
     config.image.n_channels              = 2;
     % When
     actual_filter = model.terms.interactions.colors.excitation_filter(config);
     % Then
     expected_size   = 3;     % Note: rounded to 3 for symmetry
-    expected_weight = 0.1;
+    expected_weight = 0.2;
     assertDefaultFilterSize(expected_size, actual_filter);
-    assertDefaultFilterContents(expected_size, expected_weight, actual_filter);
+    assertDefaultFilterValues(expected_size, expected_weight, actual_filter);
 end
 
 function test_default_color_excitation_filter_with_3_color_channel
@@ -60,7 +60,7 @@ function test_default_color_excitation_filter_with_3_color_channel
     expected_size   = 3;
     expected_weight = 0.1;
     assertDefaultFilterSize(expected_size, actual_filter);
-    assertDefaultFilterContents(expected_size, expected_weight, actual_filter);
+    assertDefaultFilterValues(expected_size, expected_weight, actual_filter);
 end
 
 function test_default_color_excitation_filter_with_42_color_channel
@@ -68,21 +68,76 @@ function test_default_color_excitation_filter_with_42_color_channel
     config = struct();
     config.zli.interaction.color.enabled = true;
     config.zli.interaction.color.scheme  = 'default';
-    config.zli.interaction.color.weight  = 0.5;
+    config.zli.interaction.color.weight  = 0.05;
     config.image.n_channels              = 42;
     % When
     actual_filter = model.terms.interactions.colors.excitation_filter(config);
     % Then
     expected_size   = 43;   % Note: rounded to 43 for symmetry
-    expected_weight = 0.5;
+    expected_weight = 0.05;
     assertDefaultFilterSize(expected_size, actual_filter);
-    assertDefaultFilterContents(expected_size, expected_weight, actual_filter);
+    assertDefaultFilterValues(expected_size, expected_weight, actual_filter);
 end
 
 %% TEST OPPONENT COLOR INTERACTIONS
 
+function test_opponent_color_excitation_filter_with_1_color_channel
+    % Given
+    config = struct();
+    config.zli.interaction.color.enabled = true;
+    config.zli.interaction.color.scheme  = 'opponent';
+    config.zli.interaction.color.weight  = 0.3;
+    config.image.n_channels              = 1;
+    % When/Then
+    assertExceptionThrown(...
+        @() model.terms.interactions.colors.excitation_filter(config),...
+        'MODEL:config');
+end
 
-%% UTILITIES
+function test_opponent_color_excitation_filter_with_2_color_channels
+    % Given
+    config = struct();
+    config.zli.interaction.color.enabled = true;
+    config.zli.interaction.color.scheme  = 'opponent';
+    config.zli.interaction.color.weight  = 0.2;
+    config.image.n_channels              = 2;
+    % When
+    actual_filter = model.terms.interactions.colors.excitation_filter(config);
+    % Then
+    expected_weight = 0.2;
+    assertOpponentFilterSize(actual_filter)
+    assertOpponentFilterValues(expected_weight, actual_filter)
+end
+
+function test_opponent_color_excitation_filter_with_3_color_channels
+    % Given
+    config = struct();
+    config.zli.interaction.color.enabled = true;
+    config.zli.interaction.color.scheme  = 'opponent';
+    config.zli.interaction.color.weight  = 0.1;
+    config.image.n_channels              = 3;
+    % When/Then
+    assertExceptionThrown(...
+        @() model.terms.interactions.colors.excitation_filter(config),...
+        'MODEL:config');
+end
+
+function test_opponent_color_excitation_filter_with_42_color_channels
+    % Given
+    config = struct();
+    config.zli.interaction.color.enabled = true;
+    config.zli.interaction.color.scheme  = 'opponent';
+    config.zli.interaction.color.weight  = 0.05;
+    config.image.n_channels              = 42;
+    % When
+    actual_filter = model.terms.interactions.colors.excitation_filter(config);
+    % Then
+    expected_weight = 0.05;
+    assertOpponentFilterSize(actual_filter)
+    assertOpponentFilterValues(expected_weight, actual_filter)
+end
+
+%% ASSERTIONS
 
 function assertDefaultFilterSize(expected_length, actual_filter)
     actual_size = size(actual_filter);
@@ -90,23 +145,32 @@ function assertDefaultFilterSize(expected_length, actual_filter)
     assertEqual(expected_size, actual_size);
 end
 
-function assertDefaultFilterContents(expected_length, expected_weight, actual_filter)
-% Assert filter contents (center)
+function assertDefaultFilterValues(expected_length, expected_weight, actual_filter)
+    % Assert filter contents (center)
     center_index = round(expected_length / 2);
-    actual_center_value = actual_filter(1,1,center_index);
-    expected_center_value = 1;
-    assertEqual(expected_center_value, actual_center_value);
+    assertFilterSubsetValues(1, actual_filter, center_index)
     % Assert filter contents (left)
     left_indices = 1:center_index-1;
-    actual_left_filter_values = actual_filter(1,1,left_indices);
-    actual_left_filter_values = reshape(actual_left_filter_values, 1, length(left_indices));
-    expected_left_filter_values = ones(size(left_indices)) * expected_weight;
-    assertEqual(expected_left_filter_values, actual_left_filter_values);
+    assertFilterSubsetValues(expected_weight, actual_filter, left_indices)
     % Assert filter contents (right)
     right_indices = center_index+1:expected_length;
-    actual_right_filter_values = actual_filter(1,1,right_indices);
-    actual_right_filter_values = reshape(actual_right_filter_values, 1, length(right_indices));
-    expected_right_filter_values = ones(size(right_indices)) * expected_weight;
-    assertEqual(size(expected_right_filter_values), size(actual_right_filter_values));
-    assertEqual(expected_right_filter_values, actual_right_filter_values);
+    assertFilterSubsetValues(expected_weight, actual_filter, right_indices)
+end
+
+function assertFilterSubsetValues(expected_value, filter, subset)
+    actual_filter_subset_values = reshape(filter(1,1,subset), 1, length(subset));
+    expected_filter_subset_values = ones(size(subset)) * expected_value;
+    assertEqual(expected_filter_subset_values, actual_filter_subset_values);
+end
+
+function assertOpponentFilterSize(actual_filter)
+    actual_size   = size(actual_filter);
+    expected_size = [1 1 3];
+    assertEqual(expected_size, actual_size);
+end
+
+function assertOpponentFilterValues(expected_weight, actual_filter)
+    actual_values = reshape(actual_filter, 1, 3);
+    expected_values = [expected_weight, 1, expected_weight];
+    assertEqual(expected_values, actual_values);
 end
