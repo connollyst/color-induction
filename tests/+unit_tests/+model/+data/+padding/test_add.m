870c78e7793d7adc8cb69b91092c5286afea2e51
Sean Connolly <connolly.st@gmail.com>
2014-07-22 17:38:06 +0200

    Simplified padding.add regression test

--- tests/+unit_tests/+model/+data/+padding/test_add.m
+++ tests/+unit_tests/+model/+data/+padding/test_add.m
@@ -38,32 +38,32 @@ end
 
 function assert_gx_padded(instance)
     % Given
-    [x, ~, interactions, config] = get_input(instance);
-    gx                           = model.terms.gx(x);
+    [gx, ~, interactions, config] = get_input(instance);
     % When
-    gx_padded                    = model.data.padding.add(gx, interactions, config);
+    gx_padded = model.data.padding.add(gx, interactions, config);
     % Then
-    expected                     = get_output(instance);
+    expected = get_output(instance);
     assertEqualData(gx_padded, expected.newgx_toroidal_x);
 end
 
 function assert_gy_padded(instance)
     % Given
-    [~, y, interactions, config] = get_input(instance);
-    gy                           = model.terms.gy(y);
+    [~, gy, interactions, config] = get_input(instance);
     % When
-    gy_padded                    = model.data.padding.add(gy, interactions, config);
+    gy_padded = model.data.padding.add(gy, interactions, config);
     % Then
-    expected                     = get_output(instance);
+    expected = get_output(instance);
     assertEqualData(gy_padded, expected.newgy_toroidal_y);
 end
 
 %% TEST UTILITIES
 
-function [x, y, interactions, config] = get_input(instance)
+function [gx, gy, interactions, config] = get_input(instance)
     input        = load(['data/input/add_padding_',instance,'.mat']);
     x            = input.x;
     y            = input.y;
+    gx           = model.terms.gx(x);
+    gy           = model.terms.gy(y);
     config       = get_test_config(40, 40, 3, 2);
     interactions = model.terms.get_interactions(config);
 end
