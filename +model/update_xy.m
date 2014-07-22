f190e600a7546297f61663b7fd7051fa0a7d2ac7
Sean Connolly <connolly.st@gmail.com>
2014-07-22 17:06:03 +0200

    Minor formatting

--- +model/update_xy.m
+++ +model/update_xy.m
@@ -11,8 +11,8 @@ function [x_out, y_out] = update_xy(tIitheta, x, y, norm_mask, interactions, con
 %   x_out:          The new excitatory membrane potentials.
 %   y_out:          The new inhibitory membrane potentials.
 
-    gx = model.terms.gx(x);
-    gy = model.terms.gy(y);
+    gx                     = model.terms.gx(x);
+    gy                     = model.terms.gy(y);
     [gx_padded, gy_padded] = model.data.padding.add(gx, gy, interactions, config);
     [x_ee, x_ei, y_ie]     = model.terms.get_excitation_and_inhibition(gx_padded, gy_padded, interactions, config);
     I_norm                 = model.terms.normalization(norm_mask, gx_padded, interactions.scale, config);
