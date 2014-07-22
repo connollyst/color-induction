2987e7b7a051c4318e6249b253e79e3ce6cbdc2b
Sean Connolly <connolly.st@gmail.com>
2014-07-22 17:13:57 +0200

    Removed duplicate code in padding.add.scale()
    No noticable performance impact.

--- +model/+data/+padding/+add/scale.m
+++ +model/+data/+padding/+add/scale.m
@@ -1,10 +1,10 @@
-function [gx_padded, gy_padded] = scale(x, y, scale_interactions, config)
+function gx_padded = scale(x, scale_interactions, config)
 %PADDING.ADD Add padding to prevent edge effects.
-    [ x_padded,  y_padded] = mirror_boundary(x, y, scale_interactions, config);
-    [gx_padded, gy_padded] = do_something(x_padded, y_padded, scale_interactions, config);
+    x_padded  = mirror_boundary(x, scale_interactions, config);
+    gx_padded = do_something(x_padded, scale_interactions, config);
 end
 
-function [x_toroidal, y_toroidal] = mirror_boundary(x, y, scale_interactions, config)
+function [x_toroidal, y_toroidal] = mirror_boundary(x, scale_interactions, config)
 %MIRROR_BOUNDARY Mirror the edges of the data to prevent edge effects.
     n_scales             = config.wave.n_scales;
     scale_deltas         = scale_interactions.deltas;
@@ -16,13 +16,11 @@ function [x_toroidal, y_toroidal] = mirror_boundary(x, y, scale_interactions, co
     for s=1:n_scales
         scale_delta = scale_deltas(s);
         x_toroidal{s+scale_distance} = padarray(x(:,:,:,s,:), [scale_delta,scale_delta,0], 'symmetric');
-        y_toroidal{s+scale_distance} = padarray(y(:,:,:,s,:), [scale_delta,scale_delta,0], 'symmetric');
     end
 end
 
-function [gx_toroidal, gy_toroidal] = do_something(x_toroidal, y_toroidal, scale_interactions, config)
+function x_toroidal = do_something(x_toroidal, scale_interactions, config)
 %DO_SOMETHING ..what sorcery is this?
-
     n_cols               = config.image.width;
     n_rows               = config.image.height;
     n_scales             = config.wave.n_scales;
@@ -31,16 +29,8 @@ function [gx_toroidal, gy_toroidal] = do_something(x_toroidal, y_toroidal, scale
     border_weight        = scale_interactions.border_weight;
     n_scale_interactions = scale_interactions.n_interactions;
     
-    %gx_toroidal = model.terms.gx(x_toroidal);
-    %gy_toroidal = model.terms.gy(y_toroidal);
-    gx_toroidal = x_toroidal;
-    gy_toroidal = y_toroidal;
-    
-    
     kk_tmp1_x = zeros(size(x_toroidal{scale_distance+1})); 
     kk_tmp2_x = zeros(size(x_toroidal{n_scales+scale_distance}));
-    kk_tmp1_y = zeros(size(y_toroidal{scale_distance+1})); 
-    kk_tmp2_y = zeros(size(y_toroidal{n_scales+scale_distance}));
     
     for i=1:scale_distance+1
         cols      = scale_deltas(1)+1            : scale_deltas(1)+n_cols;
@@ -53,14 +43,10 @@ function [gx_toroidal, gy_toroidal] = do_something(x_toroidal, y_toroidal, scale
         si_rows   = scale_deltas(n_scales-i+1)+1 : scale_deltas(n_scales-i+1)+n_rows;
         radius_i  = scale_distance+i;
         radius_si = n_scales+scale_distance-(i-1);
-        kk_tmp1_x(cols,rows,:)     = kk_tmp1_x(cols,rows,:)     + border_weight(i) * gx_toroidal{radius_i}(i_cols,i_rows,:);
-        kk_tmp1_y(cols,rows,:)     = kk_tmp1_y(cols,rows,:)     + border_weight(i) * gy_toroidal{radius_i}(i_cols,i_rows,:);
-        kk_tmp2_x(s_cols,s_rows,:) = kk_tmp2_x(s_cols,s_rows,:) + border_weight(i) * gx_toroidal{radius_si}(si_cols,si_rows,:);
-        kk_tmp2_y(s_cols,s_rows,:) = kk_tmp2_y(s_cols,s_rows,:) + border_weight(i) * gy_toroidal{radius_si}(si_cols,si_rows,:);
+        kk_tmp1_x(cols,rows,:)     = kk_tmp1_x(cols,rows,:)     + border_weight(i) * x_toroidal{radius_i}(i_cols,i_rows,:);
+        kk_tmp2_x(s_cols,s_rows,:) = kk_tmp2_x(s_cols,s_rows,:) + border_weight(i) * x_toroidal{radius_si}(si_cols,si_rows,:);
     end
 
-    gx_toroidal{1:scale_distance} = kk_tmp1_x;
-    gy_toroidal{1:scale_distance} = kk_tmp1_y;
-    gx_toroidal{n_scales+scale_distance+1:n_scale_interactions} = kk_tmp2_x;
-    gy_toroidal{n_scales+scale_distance+1:n_scale_interactions} = kk_tmp2_y;
+    x_toroidal{1:scale_distance} = kk_tmp1_x;
+    x_toroidal{n_scales+scale_distance+1:n_scale_interactions} = kk_tmp2_x;
 end
--- +model/+data/+padding/add.m
+++ +model/+data/+padding/add.m
@@ -1,8 +1,7 @@
-function [gx_padded, gy_padded] = add(x, y, interactions, config)
+function [x_padded, y_padded] = add(x, y, interactions, config)
 %PADDING.ADD Add padding to prevent edge effects.
-    x_padded = model.data.padding.add.color(x, interactions.color, config);
-    y_padded = model.data.padding.add.color(y, interactions.color, config);
-    [gx_padded, gy_padded] = model.data.padding.add.scale( ...
-                                x_padded, y_padded, interactions.scale, config ...
-                             );
+    x_padded = model.data.padding.add.color(x,        interactions.color, config);
+    y_padded = model.data.padding.add.color(y,        interactions.color, config);
+    x_padded = model.data.padding.add.scale(x_padded, interactions.scale, config);
+    y_padded = model.data.padding.add.scale(y_padded, interactions.scale, config);
 end
