a27df3ec11ea9554addb12668065cc01d5a417c4
Sean Connolly <connolly.st@gmail.com>
2014-07-23 15:49:21 +0200

    Corrected model.terms.Inoise use

--- +model/+terms/Ic.m
+++ +model/+terms/Ic.m
@@ -1,6 +1,6 @@
 function ic = Ic(config)
 %MODEL.TERMS.IC Spontaneous firing rate & neural noise.
 %               p.209, Li 1999
-    ic = 1.0  + model.terms.noise(config);
+    ic = 1.0  + model.terms.Inoise(config);
 end
 
