5c894b350c6373cbef55c8698c8fdbc6c8616195
Sean Connolly <connolly.st@gmail.com>
2014-07-23 14:43:01 +0200

    Removed superfluous configuration

--- +configurations/double_opponent.m
+++ +configurations/double_opponent.m
@@ -1,9 +1,4 @@
 function config = double_opponent()
-
     config     = configurations.default();
     config.zli = configurations.opponent.get_zli();
-    
-    % Decompose with an oriented wavelet transform
-    config.wave.transform = 'DWD_orient_undecimated';
-    config.wave.n_orients = 3;
 end
