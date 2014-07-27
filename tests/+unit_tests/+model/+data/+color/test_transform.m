73890bbae82d2ab0763cf88b2d7594957a87d5af
Sean Connolly <connolly.st@gmail.com>
2014-07-27 19:08:31 +0200

    Added test for L*a*b transform

--- tests/+unit_tests/+model/+data/+color/test_transform.m
+++ tests/+unit_tests/+model/+data/+color/test_transform.m
@@ -11,6 +11,15 @@ function test_no_transform
     assertEqualData(I_out, as_double(I_in));
 end
 
+function test_lab_transform
+    % Given
+    [I_in, config] = get_input('lab');
+    % When
+    I_out = model.data.color.transform(I_in, config);
+    % Then
+    assertEqualData(I_out, as_lab(I_in));
+end
+
 
 %% UTILITIES
 
@@ -28,5 +37,12 @@ function I_double = as_double(I)
     I_double = cell(size(I));
     for i=1:length(I)
         I_double{i} = im2double(I{i});
+    end
+end
+
+function I_double = as_lab(I)
+    I_double = cell(size(I));
+    for i=1:length(I)
+        I_double{i} = lab2double(applycform(I{i}, makecform('srgb2lab')));
     end
 end
