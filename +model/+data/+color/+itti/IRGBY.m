dbc5da1c9d12f714286f849f9d6684a76f7d0646
Sean Connolly <connolly.st@gmail.com>
2014-08-13 16:41:35 +0200

    Removed plotting

--- +model/+data/+color/+itti/IRGBY.m
+++ +model/+data/+color/+itti/IRGBY.m
@@ -1,14 +1,14 @@
-function [I, R, G, B, Y] = IRGBY(img)
+function [I, R, G, B, Y] = IRGBY(rgb)
 %RGBY Separate RGB image into intensity (I), red (R), green (G), blue (B),
 %     and yellow (Y) components according to the transformations defined in
 %     Itti L, Kock C, Niebur E (1998) A model of saliency-based visual
 %     attention for rabid scene analysis. IEEE Transactions on Pattern
 %     Analysis and Machiene Intelligence 20:1254-1259
-    img = im2double(img);
+    rgb = im2double(rgb);
     
-    r = img(:,:,1);
-    g = img(:,:,2);
-    b = img(:,:,3);
+    r = rgb(:,:,1);
+    g = rgb(:,:,2);
+    b = rgb(:,:,3);
     
     I = (r + b + g)/3;
     
@@ -20,14 +20,6 @@ function [I, R, G, B, Y] = IRGBY(img)
     G = utils.on(g - (r + b)/2);
     B = utils.on(b - (r + g)/2);
     Y = utils.on((r + g)/2 - abs(r - g)/2 - b);
-    
-    figure()
-    subplot(3, 2, 1), imshow(img);
-    subplot(3, 2, 2), imagesc(I), colormap('gray'), title('intensity');
-    subplot(3, 2, 3), imagesc(R), colormap('gray'), title('red');
-    subplot(3, 2, 4), imagesc(G), colormap('gray'), title('green');
-    subplot(3, 2, 5), imagesc(B), colormap('gray'), title('blue');
-    subplot(3, 2, 6), imagesc(Y), colormap('gray'), title('yellow');
 end
 
 function n = normalize(channel, I)
