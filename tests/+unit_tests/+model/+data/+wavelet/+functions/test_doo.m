c00ee0bf5f2269fb2c1a8393b8a14e9253442d9b
Sean Connolly <connolly.st@gmail.com>
2014-08-15 12:57:53 +0200

    Oriented double opponent unit tests

--- tests/+unit_tests/+model/+data/+wavelet/+functions/test_doo.m
+++ tests/+unit_tests/+model/+data/+wavelet/+functions/test_doo.m
@@ -33,6 +33,54 @@ function test_decomposition_values_when_white
     assertEqual(min(wavelets(:)), 0);
 end
 
+function test_all_scales_contain_signal
+    n_scales = 5;
+    I = little_peppers;
+    config = make_config('doo', n_scales);
+    [wavelets, ~] = model.data.wavelet.functions.doo(I, config);
+    for s=1:n_scales
+        scale = wavelets(:,:,:,s,:);
+        assertTrue(max(scale(:)) ~= 0, ...
+                    ['Expected signal in scale ', num2str(s)]);
+    end
+end
+
+function test_signal_range_zero_to_one
+    n_scales = 5;
+    I = little_peppers;
+    config = make_config('doo', n_scales);
+    [wavelets, ~] = model.data.wavelet.functions.doo(I, config);
+    for s=1:n_scales
+        scale = wavelets(:,:,:,s,:);
+        max_scale = max(scale(:));
+        min_scale = min(scale(:));
+        assertTrue(min_scale >= 0, ...
+                    ['Expected signal greater than 0 in scale ', num2str(s), ...
+                     ', found: ', num2str(min_scale)]);
+        assertTrue(max_scale <= 1, ...
+                    ['Expected signal less than 1 in scale ', num2str(s), ...
+                     ', found: ', num2str(max_scale)]);
+    end
+end
+
+function test_higher_scales_contain_weaker_signal
+    n_scales = 5;
+    I = little_peppers;
+    config = make_config('doo', n_scales);
+    [wavelets, ~] = model.data.wavelet.functions.doo(I, config);
+    running_max = Inf;
+    for s=1:n_scales
+        scale = wavelets(:,:,:,s,:);
+        current_max = max(scale(:));
+        assertTrue(current_max < running_max,   ...
+                    ['Expected signal in scale ', num2str(s),   ...
+                     ' greater than ', num2str(s-1),            ...
+                     ', was ', num2str(current_max),            ...
+                     ' compared to ', num2str(running_max)      ...
+                     ]);
+    end
+end
+
 %% TEST UTILITIES
 
 function I = make_black_I()
