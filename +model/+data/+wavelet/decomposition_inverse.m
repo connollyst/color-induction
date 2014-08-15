009096e4477e58c6a918f9382a1aa717e88a50bb
Sean Connolly <connolly.st@gmail.com>
2014-08-15 13:28:57 +0200

    Added DOo inverse transform

--- +model/+data/+wavelet/decomposition_inverse.m
+++ +model/+data/+wavelet/decomposition_inverse.m
@@ -1,8 +1,7 @@
 function I_out = decomposition_inverse(wavelets, residuals, config)
 %WAVELET.DECOMPOSITION_INVERSE Perform the inverse wavelet transform
-% N.B.: we only perform the inverse wavelet transform of the frames used for computing the
-% mean, and NOT of all the sequence!
-    % number of frames (among the last ones) we use when we consider the mean
+% Note: we only perform the inverse wavelet transform of the frames used
+%       for computing the mean, and NOT of the full sequence.
     n_membr = config.zli.n_membr;
     I_out   = cell(n_membr,1);
     inverse_transform = str2func(['model.data.wavelet.functions.',config.wave.transform,'_inverse']);
