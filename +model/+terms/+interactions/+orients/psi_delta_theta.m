4839e3d348cb4c4d458687d86ef0f37cdb020cc3
Sean Connolly <connolly.st@gmail.com>
2014-08-17 17:43:44 +0200

    Added PsiDeltaTheta for SO & DO model

--- +model/+terms/+interactions/+orients/psi_delta_theta.m
+++ +model/+terms/+interactions/+orients/psi_delta_theta.m
@@ -1,18 +1,24 @@
 function pdt = psi_delta_theta(config)
 % Orientation interaction weights
     n_orients = config.wave.n_orients;
-    if n_orients == 3 || n_orients == 4
-        % Based on Z. Li 1999, p209
-        a               = pi/4;
-        b               = pi/2;
-        delta_theta     = [0 a b; a 0 a ; b a 0];
-        pdt = cos(abs(delta_theta)).^6;
-    else
-        if n_orients == 1
-            % Non-directional 
+    switch n_orients
+        case 1
+            % Non-directional single opponent cells
             pdt = 1;
-        else
+        case 3
+            % Oriented double opponent cells
+            % Based on Z. Li 1999, p209
+            a               = pi/4;
+            b               = pi/2;
+            delta_theta     = [0 a b; a 0 a ; b a 0];
+            pdt = cos(abs(delta_theta)).^6;
+        case 4
+            % Both single and double opponent cells
+            a               = pi/4;
+            b               = pi/2;
+            delta_theta     = [0 a b 1; a 0 a 1; b a 0 1; 0 0 0 1];
+            pdt = cos(abs(delta_theta)).^6;
+        otherwise
             error('Orientations not supported: %i', n_orients)
-        end
     end
 end
