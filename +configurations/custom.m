a1d33c82c276b6adbbcca67cae6c9b1a3e544d53
Sean Connolly <connolly.st@gmail.com>
2014-08-17 17:43:09 +0200

    Added a 'custom' config template
    To encourage folks to not modify the core configuration files.

---
+++ +configurations/custom.m
@@ -0,0 +1,18 @@
+function config = custom()
+% This is a custom configuration template to help you get started.
+% It is better to copy and modify this file, rather than modifying the core
+% configuration files themselves.
+%
+% To run the model with your custom configurations, simply provide the name
+% of the function as the last argument when applying the model, e.g.:
+% 
+%   out = process_image(in, 'rgb', 20, 'custom');
+% 
+
+    config = configurations.default;
+    
+    % Custom Configurations:
+    
+    % e.g.: config.wave.transform = 'dwt'
+    
+end
