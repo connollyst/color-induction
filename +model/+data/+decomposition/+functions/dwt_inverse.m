d751e502a5ef88a9daba1329a08ffbe523317e8e
Sean Connolly <sean@MacBook-Air-de-Sean.local>
2014-08-24 21:51:31 -0400

    Corrected dwt_inverse

---
+++ +model/+data/+decomposition/+functions/dwt_inverse.m
@@ -0,0 +1,23 @@
+function recovered = dwt_inverse(wavelets, residuals)
+    n_scales  = size(residuals, 4);
+    recovered = residuals(:,:,:,n_scales);
+    for s = n_scales:-1:1
+        switch size(wavelets, 5)
+            case 1
+                recovered = recovered       ...
+                    + wavelets(:,:,:,s,1);      % non-oriented
+            case 3
+                % TODO why is this sensitive to the order??
+                recovered = recovered       ...
+                    + wavelets(:,:,:,s,1)   ... % horizontal
+                    + wavelets(:,:,:,s,3)   ... % vertical
+                    + wavelets(:,:,:,s,2);      % diagonal
+            case 4
+                recovered = recovered       ...
+                    + wavelets(:,:,:,s,1)   ... % horizontal
+                    + wavelets(:,:,:,s,2)   ... % diagonal
+                    + wavelets(:,:,:,s,3)   ... % vertical
+                    + wavelets(:,:,:,s,4);      % non-oriented
+        end
+    end
+end
