868d0e321426a12c31404546946a28a2097ed302
Sean Connolly <sean@MacBook-Air-de-Sean.local>
2014-08-25 20:40:02 -0400

    Updated configs to use DWT by default

--- +configurations/+components/get_wave.m
+++ +configurations/+components/get_wave.m
@@ -2,15 +2,15 @@ function wave = get_wave()
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% wavelets' parameters %%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    wave.transform    = 'opponent'; % decomposition function
-                                    %  (corresponds to function in model.wavelet.functions)
-                                    
-    wave.n_orients    = 4;          % number of orientations
-                                    %  (3 double opponent and 1 single opponent)
-                                    
-    wave.n_scales     = 0;          % number of decompositon scales
-                                    % (if 0: code calculates it automatically)
-                                    
-    wave.mida_min     = 32;         % size of the last wavelet plane to process (Should be a power of 2 and >= 32)
-                                    %  (see zli.fin_scale_offset parameter in order to include or not residual plane)
+    wave.transform    = 'dwt';  % decomposition function
+                                %  (corresponds to function in model.wavelet.functions)
+                                
+    wave.n_orients    = 4;      % number of orientations
+                                %  (3 double opponent and 1 single opponent)
+                                
+    wave.n_scales     = 0;      % number of decompositon scales
+                                %  (if 0: code calculates it automatically)
+                                
+    wave.mida_min     = 32;     % size of the last wavelet plane to process (Should be a power of 2 and >= 32)
+                                %  (see zli.fin_scale_offset parameter in order to include or not residual plane)
 end
--- +configurations/wavelet_opponent.m
+++
@@ -1,7 +0,0 @@
-function config = wavelet_opponent()
-    config = configurations.default();
-    config.rf.single      = true;
-    config.rf.double      = true;
-    config.wave.n_orients = 4;  % DO and SO
-    config.wave.transform = 'dwt';
-end
