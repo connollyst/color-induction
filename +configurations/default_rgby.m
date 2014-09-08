b7dae4a0574db29b35b82d787867027d31216f47
Sean Connolly <sean@sean-air.local>
2014-09-08 11:39:50 -0400

    RGBY uses only 'on' activity

--- +configurations/default_rgby.m
+++ +configurations/default_rgby.m
@@ -1,6 +1,6 @@
 function config = default_rgby()
     config = configurations.default;
     config.image.transform = 'none';
-    config.zli.ON_OFF      = 'separate';
+    config.zli.ON_OFF      = 'on';
     config.wave.transform  = 'opponent';
 end
